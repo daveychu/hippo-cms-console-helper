926dbd6edffd76951daf3ebc92fc92cf2e3cd162
Davey Chu <davey.chu@gmail.com>
2017-09-09 13:54:56 +0200

    Keep the tree view in sync with the detail view

---
+++ README.md
@@ -0,0 +1,8 @@
+# hippo-cms-console-helper
+A hacky Chrome plugin that keeps the Hippo CMS console tree view in sync with the detail view
+
+# How to use
+See https://developer.chrome.com/extensions/getstarted "Load the extension"
+
+# DEMO
+![DEMO](https://github.com/daveychu/hippo-cms-console-helper/blob/master/demo.gif)
---
+++ myscript.js
@@ -0,0 +1,35 @@
+var currentPage = window.location.href;
+var container = $('#yui-gen11 > div > div');
+
+setInterval(function()
+{
+    if (currentPage != window.location.href)
+    {
+        currentPage = window.location.href;
+        var stringsToFind = getParameterByName('path', currentPage).split('/');
+
+        var divs = container.find("span:contains('" + stringsToFind[1] + "')").parent().parent().parent().nextAll();
+        for (var i = 2, len = stringsToFind.length; i < len; i++) {
+       	  console.log(divs);
+          divs = divs.filter(function(index){
+          	return divs.eq(index).find("span:contains('" + stringsToFind[i] + "')").length > 0;
+          }).first().nextAll();
+		}
+
+		var scrollTo = divs.first();
+
+		//scroll element to center
+    var offset = scrollTo.offset().top - container.offset().top + container.scrollTop() - (container.height() / 2);
+    container.animate({scrollTop: offset});
+	}
+}, 250);
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
